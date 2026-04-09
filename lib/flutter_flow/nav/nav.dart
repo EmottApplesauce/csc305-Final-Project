3a71c19a2ee4b3ff817d4622440a73b8ce1a7274
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-04-09 12:48:02 +0000

    Updating to latest FlutterFlow output.

--- lib/flutter_flow/nav/nav.dart
+++ lib/flutter_flow/nav/nav.dart
@@ -79,14 +79,14 @@ GoRouter createRouter(AppStateNotifier appStateNotifier, [Widget? entryPage]) =>
       navigatorKey: appNavigatorKey,
       errorBuilder: (context, state) => appStateNotifier.loggedIn
           ? entryPage ?? BaseWidget()
-          : AuthenticationPageWidget(),
+          : LoginPageWidget(),
       routes: [
         FFRoute(
           name: '_initialize',
           path: '/',
           builder: (context, _) => appStateNotifier.loggedIn
               ? entryPage ?? BaseWidget()
-              : AuthenticationPageWidget(),
+              : LoginPageWidget(),
         ),
         FFRoute(
           name: AuthenticationPageWidget.routeName,
@@ -307,7 +307,7 @@ class FFRoute {
 
           if (requireAuth && !appStateNotifier.loggedIn) {
             appStateNotifier.setRedirectLocationIfUnset(state.uri.toString());
-            return '/authenticationPage';
+            return '/loginPage';
           }
           return null;
         },
